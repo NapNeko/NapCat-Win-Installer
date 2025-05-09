58acbbbc639c310ec400f8dc95bb8f51e38ee20d
手瓜一十雪 <nanaeonn@outlook.com>
2025-05-09 19:27:02 +0800

    fix

--- installer/main.cpp
+++ installer/main.cpp
@@ -572,7 +572,6 @@ int main()
         printInfo(L"开始下载NapCat...");
 
         std::vector<std::wstring> mirrorUrls = {
-            L"https://gh.llkk.cc/https://github.com/NapNeko/NapCatQQ/releases/latest/download/NapCat.Framework.zip",
             L"https://github.moeyy.xyz/https://github.com/NapNeko/NapCatQQ/releases/latest/download/NapCat.Framework.zip",
             L"https://ghp.ci/https://github.com/NapNeko/NapCatQQ/releases/latest/download/NapCat.Framework.zip",
             L"https://gh.api.99988866.xyz/https://github.com/NapNeko/NapCatQQ/releases/latest/download/NapCat.Framework.zip",
